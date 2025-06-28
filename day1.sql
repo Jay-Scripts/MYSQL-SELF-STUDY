763370db843f7fa03814d1aba118c5ade40e567d
JayScriptt <corneliogatbontonjr21@gmail.com>
2025-06-29 01:07:23 +0800

    .

--- day1.sql
+++ day1.sql
@@ -23,3 +23,21 @@ enroll_id int AUTO_INCREMENT not null,
     FOREIGN KEY (student_id) REFERENCES students(student_id),
     FOREIGN key (course_id) REFERENCES  courses(course_id) 
 );
+
+--act 3 
+--🔹 Part A – Basic SELECT (5 points each)
+--1. Display all book titles and their authors.
+SELECT title, AUTHOR from books;
+--2. Display all VIP members only.
+SELECT * FROM members WHERE membership_type ='VIP';
+--3. Show only the 2 most recently borrowed books (BORROWED_DATE DESC)
+--ddnt know that
+--4. Show full names of members with the titles of books they borrowed.
+SELECT MEMBER_ID FROM borrowed_books JOIN ON borrowed_books members.FULL_NAME = MEMBER_ID;
+--5. Display all borrowed records including member name, book title, and borrowed date.
+
+🔹 Part C – Bonus Challenge (optional, +5)
+SELECT
+MEMBERS.FULL_NAME,
+BOOKS.TITLE FROM borrowed_books
+JOIN members ON borrowed_books.MEMBER_ID = members.MEMBER_ID;
